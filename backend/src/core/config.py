e2b445ada69e4355e48929f7b09ab86d5dbe638c
Tu Nombre <guerrayosnel03@gmail.com>
2025-04-21 08:12:43 -0400

    Ultima version presentando a yeimi

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # Usa variable de entorno (y un valor por defecto vacío para producción)
-    DATABASE_URL: str = ""  # <-- Valor por defecto vacío
+    DATABASE_URL: str =  "postgresql://postgre:@localhost:5432/nombre_de_base_datos"
     SECRET_KEY: str = "1234"
     ALGORITHM: str = "HS256"
 
