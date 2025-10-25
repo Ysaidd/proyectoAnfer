16a87eb3eba9e6c4f45ce2688babde48414881aa
Ysaidd <yosnelguerrareydev@gmail.com>
2025-10-24 22:29:38 -0400

    corregidos los fetch

--- backend/app/main.py
+++ backend/app/main.py
@@ -50,7 +50,7 @@ origins = [
     "http://localhost:8000",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:8000",
-    "https://anfer-react.onrender.com"
+    "https://anfer-react.onrender.com/"
     # "https://tu-frontend.vercel.app",
 ]
 app.add_middleware(
